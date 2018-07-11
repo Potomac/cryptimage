fa8cbe5e5c0320e0aea25c2c1b670aa7dd481d35
Mannix54 <ibsoftware@free.fr>
2018-07-12 01:06:57 +0200

    Revert "resizing image"
    This reverts commit a0f826c777b62eb4cdbe731c6518b20cc6e55fe5.

--- CryptImage_Dev/src/com/ib/cryptimage/core/SplitFrames.java
+++ CryptImage_Dev/src/com/ib/cryptimage/core/SplitFrames.java
@@ -310,12 +310,9 @@ public class SplitFrames {
 //	        finalw = (int)(finalh * factor  );
 //	    }   
 
-	    BufferedImage resizedImg = new BufferedImage(finalw, finalh, BufferedImage.TYPE_3BYTE_BGR);
+	    BufferedImage resizedImg = new BufferedImage(finalw, finalh, BufferedImage.TRANSLUCENT);
 	    Graphics2D g2 = resizedImg.createGraphics();
-	    //g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-	    g2.setRenderingHint(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);
-	    g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);
+	    g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
 	    g2.drawImage(src, 0, 0, finalw, finalh, null);
 	    g2.dispose();
 	    
